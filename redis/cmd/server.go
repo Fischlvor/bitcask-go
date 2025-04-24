27310380e886cac46887d38483aa6c80546fcd19
Fischlvor <88753413+Fischlvor@users.noreply.github.com>
2025-04-24 12:09:56 +0800

    Update server.go

--- redis/cmd/server.go
+++ redis/cmd/server.go
@@ -56,22 +56,3 @@ func (svr *BitcaskServer) close(conn redcon.Conn, err error) {
 	_ = svr.server.Close()
 }
 
-// redis 协议解析的示例
-//func main() {
-//	conn, err := net.Dial("tcp", "localhost:6379")
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	// 向 Redis 发送一个命令
-//	cmd := "set k-name-2 bitcask-kv-2\r\n"
-//	conn.Write([]byte(cmd))
-//
-//	// 解析 Redis 响应
-//	reader := bufio.NewReader(conn)
-//	res, err := reader.ReadString('\n')
-//	if err != nil {
-//		panic(err)
-//	}
-//	fmt.Println(res)
-//}
